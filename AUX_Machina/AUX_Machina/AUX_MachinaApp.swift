3384f7e8cc5eb0eb4b77a0e3df5d20cad8d53ef6
Your Name <ggboots.99@gmail.com>
2025-01-04 22:01:18 +1100

    initial commit

---
+++ AUX_Machina/AUX_Machina/AUX_MachinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AUX_MachinaApp.swift
+//  AUX_Machina
+//
+//  Created by Grahm on 4/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct AUX_MachinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AUX_Machina/AUX_MachinaTests/AUX_MachinaTests.swift
@@ -0,0 +1,17 @@
+//
+//  AUX_MachinaTests.swift
+//  AUX_MachinaTests
+//
+//  Created by Grahm on 4/1/2025.
+//
+
+import Testing
+@testable import AUX_Machina
+
+struct AUX_MachinaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
